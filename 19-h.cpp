405ed11cadbbee50f7d989c1b66efdd170c4ed06
asandstar <aiziqing2020@163.com>
2025-02-16 11:08:32 +0800

    hard 19 success and hard 'calculate UCC substring' failed

---
+++ 19-h.cpp
@@ -0,0 +1,50 @@
+// https : // www.marscode.cn/practice/enno1xy36v12wk?problem_id=7424418560665370668
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string solution(int n, int k, string s)
+{
+    // PLEASE DO NOT MODIFY THE FUNCTION SIGNATURE
+    // write code here
+    for (int i = 0; i < n && k > 0; ++i)
+    {
+        // 找到当前最小的字符及其位置
+        int min_index = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (s[j] < s[min_index])
+            {
+                min_index = j;
+            }
+        }
+
+        // 计算将最小字符移动到当前位置所需的交换次数
+        int swaps = min_index - i;
+
+        // 如果交换次数不超过 k，则进行交换
+        if (swaps <= k)
+        {
+            // 将最小字符移动到当前位置
+            while (min_index > i)
+            {
+                swap(s[min_index], s[min_index - 1]);
+                min_index--;
+            }
+            // 更新剩余的交换次数
+            k -= swaps;
+        }
+    }
+
+    return s;
+}
+
+int main()
+{
+    cout << (solution(5, 2, "01010") == "00101") << endl;
+    cout << (solution(7, 3, "1101001") == "0110101") << endl;
+    cout << (solution(4, 1, "1001") == "0101") << endl;
+    return 0;
+}
